1fdae5476c0d893c03b742a88d43e811a0a63118
BxRay95 <bray387@outlook.com>
2023-03-30 16:09:57 -0700

    story 1 complete

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,37 @@
+class AnimalsController < ApplicationController
+    def show
+        animals = Animal.find(params[:id])
+        render json: animals
+    end
+    
+    def create 
+        animal = Animal.create(animal_params)
+        if animal.valid?
+            render json: animal
+        else 
+            render json: animal.errors
+        end
+
+    def update
+        animal = Animal.find(params[:id])
+        animal.update(animal_params)
+        if animal.valid?
+            render json: animal
+        else 
+            render json: animal.errors 
+        end
+    end
+
+    def destroy 
+        animal = Animal.find(params[:id])
+        if animal.destroy 
+            render json: animal
+        else 
+            render json: animal.errors
+        end
+    end
+private 
+    def animal_params
+        params.require(:animal).permit(:name, :binomial)
+    end
+end
