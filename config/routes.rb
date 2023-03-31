ac195c52f7cea9f5c940e423c094543450b0ace5
BxRay95 <bray387@outlook.com>
2023-03-31 10:54:21 -0700

    story 3 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
+  get '/sightings/:start_date/:end_date' => 'sightings#index'
   resources :sightings
   resources :animals
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
 end
